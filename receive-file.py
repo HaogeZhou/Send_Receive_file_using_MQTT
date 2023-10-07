29e00ce29bc7e9eee4b6ef9b01ea7aa33d18225c
Haoge Zhou <88472118+hankge@users.noreply.github.com>
2023-10-07 16:43:45 -0700

    Add files via upload

--- receive-file.py
+++ receive-file.py
@@ -1,26 +1,22 @@
-#! c:\python34\python3
 #!/usr/bin/env python
-##demo code provided by Steve Cope at www.steves-internet-guide.com
-##email steve@steves-internet-guide.com
-##Free to use for any purpose
-##If you like and use this code you can
-##buy me a drink here https://www.paypal.me/StepenCope
-""" Send File Using MQTT """
+
 import time
 import paho.mqtt.client as paho
 import hashlib
-broker="broker.hivemq.com"
-# broker="iot.eclipse.org"
-# broker="192.168.1.158"
+
+""" Send File Using MQTT """
+# broker="broker.hivemq.com"
+# broker= "ip_address"
+broker="localhost"
 filename="output.bin"
 #filename="chinese-proverb.jpg"
 #filename="send-receive-file.7z"
-topic="data/files"
+topic="/data"
 qos=1
-# data_block_size=2000
+
 file_out="copy-"+filename
 fout=open(file_out,"wb") #use a different filename
-# for outfile as I'm running sender and receiver together
+
 
 def process_message(msg):
    """ This is the main receiver code
@@ -31,7 +27,7 @@ def process_message(msg):
       print("found header")
       msg_in=msg.decode("utf-8")
       msg_in=msg_in.split(",,")
-      if msg_in[0]=="end": #is it really last packet?
+      if msg_in[0]=="end": 
          in_hash_final=in_hash_md5.hexdigest()
          if in_hash_final==msg_in[2]:           
             print("File copied OK -valid hash  ",in_hash_final)
@@ -72,7 +68,6 @@ client.mid_value=None
 #####
 print("connecting to broker ",broker)
 client.connect(broker)#connect
-#client.loop_start() #start loop to process received messages
 print("subscribing ")
 client.subscribe(topic)#subscribe
 time.sleep(2)
@@ -83,7 +78,6 @@ run_flag=True
 while run_flag:
    client.loop(00.1)  #manual loop
    pass
-client.disconnect() #disconnect
-#client.loop_stop() #stop loop
+
 fout.close()
 
--- send-file .py
+++ send-file .py
@@ -1,59 +1,27 @@
-#! c:\python34\python3
+
 #!/usr/bin/env python
-##demo code provided by Steve Cope at www.steves-internet-guide.com
-##email steve@steves-internet-guide.com
-##Free to use for any purpose
-##If you like and use this code you can
-##buy me a drink here https://www.paypal.me/StepenCope
-""" Send File Using MQTT """
+
 import time
 import paho.mqtt.client as paho
 import hashlib
-broker="broker.hivemq.com"
-# broker="iot.eclipse.org"
-# broker="192.168.1.158"
+
+""" Send File Using MQTT """
+
+# broker="broker.hivemq.com"
+broker="localhost"
+
 filename="output.bin"
-#filename="chinese-proverb.jpg"
-#filename="send-receive-file.7z"
-topic="data/files"
+#filename="output.txt"
+
+topic="/data"
 qos=1
 data_block_size=200000
-fo=open(filename,"rb")
-# file_out="copy-"+filename
-# fout=open(file_out,"wb") #use a different filename
-# for outfile as I'm running sender and receiver together
 
-def process_message(msg):
-   """ This is the main receiver code
-   """
-   print("received ")
-   if len(msg)==200: #is header or end
-      msg_in=msg.decode("utf-8")
-      msg_in=msg_in.split(",,")
-      if msg_in[0]=="end": #is it really last packet?
-         in_hash_final=in_hash_md5.hexdigest()
-         if in_hash_final==msg_in[2]:
-            print("File copied OK -valid hash  ",in_hash_final)
-         else:
-            print("Bad file receive   ",in_hash_final)
-         return False
-      else:
-         if msg_in[0]!="header":
-            in_hash_md5.update(msg)
-            return True
-         else:
-            return False
-   else:
-      in_hash_md5.update(msg)
-      return True
-#define callback
-# def on_message(client, userdata, message):
-#    #time.sleep(1)
-#    #print("received message =",str(message.payload.decode("utf-8")))
-#    if process_message(message.payload):
-#       fout.write(message.payload)
+data_output=open(filename,"rb")
+
+
 def on_publish(client, userdata, mid):
-    #logging.debug("pub ack "+ str(mid))
+ 
     client.mid_value=mid
     client.puback_flag=True  
 
@@ -63,7 +31,7 @@ def wait_for(client,msgType,period=0.25,wait_time=40,running_loop=False):
     wcount=0
     #return True
     while True:
-        #print("waiting"+ msgType)
+      
         if msgType=="PUBACK":
             if client.on_publish:        
                 if client.puback_flag:
@@ -78,6 +46,7 @@ def wait_for(client,msgType,period=0.25,wait_time=40,running_loop=False):
             print("return from wait loop taken too long")
             return False
     return True 
+
 def send_header(filename):
    header="header"+",,"+filename+",,"
    header=bytearray(header,"utf-8")
@@ -108,7 +77,6 @@ def c_publish(client,topic,out_message,qos):
 
 client= paho.Client("client-001")  #create client object client1.on_publish = on_publish                          #assign function to callback client1.connect(broker,port)                                 #establish connection client1.publish("data/files","on")  
 ######
-#client.on_message=on_message
 client.on_publish=on_publish
 client.puback_flag=False #use flag in publish ack
 client.mid_value=None
@@ -129,7 +97,7 @@ in_hash_md5 = hashlib.md5()
 bytes_out=0
 
 while Run_flag:
-   chunk=fo.read(data_block_size)
+   chunk=data_output.read(data_block_size)
    if chunk:
       out_hash_md5.update(chunk) #update hash
       out_message=chunk
@@ -150,5 +118,5 @@ print("bytes sent =",bytes_out)
 time.sleep(10)
 client.disconnect() #disconnect
 client.loop_stop() #stop loop
-# fout.close()
-fo.close()
+
+data_output.close()
